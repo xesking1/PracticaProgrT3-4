20daa0b9d345c40ee1ce2148dde0cadb8c89ee7d
frank <franciscojosesilva@paucasesnovescifp.cat>
2025-11-24 03:22:07 +0100

    23-11

--- src/MaquinaExp.java
+++ src/MaquinaExp.java
@@ -3,32 +3,32 @@ public class MaquinaExp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean logged = false;
-        int menuUsuaris;
+        char menuUsuaris;
         do {
             System.out.println("Config usuaris\n\n1. Registrar usuaris\n2. Login\n");
-            menuUsuaris = sc.nextInt();
+            menuUsuaris = sc.next().charAt(0);
+            sc.nextLine();
             boolean contraValida = false;
             boolean contraAdmValida = false;
             switch (menuUsuaris) {
-                case 1:
+                case '1':
                     System.out.print("\nPosa-li un nom al teu usuari: ");
                     String usuari = sc.nextLine();
-                    sc.next();
                     char ch;
-                    boolean digit = false;
-                    boolean simbol = false;
+                    boolean digit;
+                    boolean simbol;
                     while (!contraValida) {
                         System.out.print("Assigna una contrasenya: ");
                         String contrasenya = sc.next();
                         if (contrasenya.length() < 4) { // controla llargària de la contrasenya i reinicia el bucle si és curta
                             System.out.println("Ha de contenir com a mínim 4 caràcters."); continue; }
-
+                        digit = simbol = false;
                         for (int i = 0 ; i < contrasenya.length() ; i++) {
                             ch = contrasenya.charAt(i);
                             if (ch >= '!' && ch <= '~') { // controla que siguin símbols, nombres o lletres i es fan subfiltres a dins
-                                if (ch >= '0' && ch <= '9' && !digit) { digit = true; } // comprova el dígit. si ja ho ha fet, no es repeteix
-                                else if (!Character.isLetter(ch) && !simbol) { simbol = true; } // comprova el símbol agafant tot el que està dins
-                                // el rang de l'if pare (símbols, nombres i lletres), que no sigui lletra ni tampoc nombre, que es controla a l'if anterior
+                                if (ch >= '0' && ch <= '9' && !digit) { digit = true; } // comprova el dígit. si ja ho ha fet, no es repeteix per el !digit
+                                else if (!Character.isLetterOrDigit(ch) && !simbol ) { simbol = true; }  // comprova el símbol amb la funció isLetterOrDigit,
+                                // el qual només deixa símbols tinguent en compte el rang del primer if
                             } else { // caràcters invàlids per a la contrasenya
                                 System.out.println("La contrasenya té caràcters invàlids."); break;
                             }
@@ -44,14 +44,13 @@ public class MaquinaExp {
                     while (!contraAdmValida) {
                         System.out.print("Contrasenya registrada per a l'usuari " + usuari + ".\n\nÉs necessari crear un usuari admin addicionalment. Quina contrasenya li vols assignar? ");
                         String contraAdmin = sc.next();
-                        digit = false;
-                        simbol = false;
+                        digit = simbol = false;
                         for (int i = 0 ; i < contraAdmin.length() ; i++) {
                             ch = contraAdmin.charAt(i);
                             if (ch >= '!' && ch <= '~') {
                                 if (ch >= '0' && ch <= '9' && !digit) { digit = true; }
-                                else if (!Character.isLetter(ch) && !simbol) { simbol = true; }
-                            } else { // caràcters invàlids per a la contrasenya
+                                else if (!Character.isLetterOrDigit(ch) && !simbol ) { simbol = true; }
+                            } else {
                                 System.out.println("La contrasenya té caràcters invàlids."); break;
                             }
                         }
@@ -63,11 +62,11 @@ public class MaquinaExp {
                         }
                     }
                     break;
-                case 2:
+                case '2': // login
                     if (!contraAdmValida) { System.out.println("Abans de fer el login has de crear els usuaris.\n"); continue; }
                     else { System.out.println("ola"); }
                 default:
-                    System.out.println("Opció invàlida."); break;
+                    System.out.println("Opció invàlida. Tria una de les del menú.\n"); break;
             }
         } while (!logged);
 
